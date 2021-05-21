014494d360cf23e7639b886493ba5f3b447f312b
Bkeane201 <bkeane117@gmail.com>
2021-05-20 20:37:50 -0700

    completed challenge 3

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,51 +6,17 @@
 //
 
 import SwiftUI
-import MapKit
 import LocalAuthentication
 
 struct ContentView: View {
-    @State private var centerCoordinate = CLLocationCoordinate2D()
-    @State private var selectedPlace: MKPointAnnotation?
+
+    @State private var isUnlocked = false
+    @State private var unlockError = false
     
-    @State private var showingPlaceDetails = false
-    @State private var showingEditScreen = false
-    
-    @State private var isUnlocked = false 
-    // array of locations
-    @State private var locations = [CodableMKPointAnnotation]()
     var body: some View {
         ZStack {
             if isUnlocked {
-                MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
-                    .edgesIgnoringSafeArea(.all)
-                Circle()
-                    .fill(Color.blue)
-                    .opacity(0.3)
-                    .frame(width: 32, height: 32)
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            // create a new location
-                            let newLocation = CodableMKPointAnnotation()
-                            newLocation.coordinate = self.centerCoordinate
-                            newLocation.title = "Example location"
-                            self.locations.append(newLocation)
-                            self.selectedPlace = newLocation
-                            self.showingEditScreen = true
-                        }) {
-                            Image(systemName: "plus")
-                        }
-                        .padding()
-                        .background(Color.black.opacity(0.75))
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
-                        .padding(.trailing)
-                    }
-                }
+                MapContent()
             } else {
                 Button("Unlock Places") {
                     self.authenticate()
@@ -61,61 +27,34 @@ struct ContentView: View {
                 .clipShape(Capsule())
             }
         }
-        .alert(isPresented: $showingPlaceDetails) {
-            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: . default(Text("OK")), secondaryButton: .default(Text("Edit")){
-                // edit this place in here
-                self.showingEditScreen = true
-            })
-        }
-        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
-            if self.selectedPlace != nil {
-                EditView(placemark: self.selectedPlace!)
-            }
-        }
-        .onAppear(perform: loadData)
-    }
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    func loadData() {
-        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
         
-        do {
-            let data = try Data(contentsOf: filename)
-            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
-        } catch {
-            print("Unable to load saved data")
+        .alert(isPresented: $unlockError) {
+            Alert(title: Text("Error, Bad Login Credentials"), message: Text("Please try again"), dismissButton: .cancel())
         }
-    }
-    func saveData() {
-        do {
-            let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
-            let data = try JSONEncoder().encode(self.locations)
-            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
-        } catch {
-            print("unable to save data.")
-        }
+ 
     }
     func authenticate() {
         let context = LAContext()
+        //context.localizedFallbackTitle = "Use Passcode"
         var error: NSError?
         
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
             //this string is used for touch ID, where the one in infoPlist is used for face ID
             let reason = "please authenticate yourself to unlock your places"
             
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authentication in
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, authentication in
             
             DispatchQueue.main.async {
                 if success {
                     self.isUnlocked = true
                 } else {
-                    // error
+                    //self.unlockError = true
+                    print(error?.localizedDescription ?? "Failed to authenticate")
                 }
             }
         }
         } else {
+            print(error?.localizedDescription ?? "No Biometrics")
             // no biometrics
         }
     }
